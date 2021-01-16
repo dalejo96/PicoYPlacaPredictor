237dfdcc4576f52fc90f26e129346704d447b8df
dalejo96 <77456654+dalejo96@users.noreply.github.com>
2021-01-16 11:32:34 -0500

    following good practices using conditionals and avoid unnecesary variables (#4)

--- PicoYPlacaPredictor/PicoYPlacaPredictor/Methods.cs
+++ PicoYPlacaPredictor/PicoYPlacaPredictor/Methods.cs
@@ -16,19 +16,19 @@ namespace PicoYPlacaPredictor
             //Wednesday 3 (Plates 5/6)
             //Thursday 4 (Plates 7/8)
             //Friday 5 (Plates 9/0)
-            if (last==1 | last==2)
+            if (last==1 || last ==2)
             {
                 return 1;
             }
-            else if (last == 3 | last == 4)
+            else if (last == 3 || last == 4)
             {
                 return 2;
             }
-            else if (last == 5 | last == 6)
+            else if (last == 5 || last == 6)
             {
                 return 3;
             }
-            else if (last == 7 | last == 8)
+            else if (last == 7 || last == 8)
             {
                 return 4;
             }
@@ -53,32 +53,33 @@ namespace PicoYPlacaPredictor
             //splitter[1] minutos
             int hours = int.Parse(splitterTime[0]);
             int minutes = int.Parse(splitterTime[1]);
-            bool globalvalidationOfTime;
             //set the ranges of PicoYPlaca Restriction 
             //Hours: 7:00 - 9:30 / 16:00 - 19:30
-            if ((hours<=9 & hours>=7)|(hours <= 19 & hours >= 16))
+            if ((hours<=9 && hours >=7) || (hours <= 19 && hours >= 16))
             {
-                if (hours==9 | hours ==19)
+                //validate the 30 minutos of 9 and 19
+                if (hours==9 || hours ==19)
                 {
-                    if ((minutes <= 30 & minutes >= 0))
+                    if (minutes <= 30)
                     {
-                        globalvalidationOfTime = true;
+                        //true = in the restriction's time
+                        return true;
                     }
                     else
                     {
-                        globalvalidationOfTime = false;
+                        //false = not in the restriction's time
+                        return false;
                     }
                 }
                 else
                 {
-                    globalvalidationOfTime = true;
+                    return true;
                 }
             }
             else
             {
-                 globalvalidationOfTime = false;
+                return false;
             }
-            return globalvalidationOfTime;
         }
         public static bool CanRoad(PicoPlaca obj)
         {
@@ -96,7 +97,7 @@ namespace PicoYPlacaPredictor
                 dayAndPlateEqual = false;
             }
             //validate if dayAndPlateEqual and TimeRestriction are true then car cant road, else car cant road
-            if (TimeRestriction & dayAndPlateEqual)
+            if (TimeRestriction && dayAndPlateEqual)
             {
                 return false;
             }
